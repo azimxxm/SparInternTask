d208937be32ad2239a065944ee727df6579faf55
azimxxm <lifehackss.2021@gmail.com>
2024-08-10 21:57:29 +0500

    Initial Commit

---
+++ SparInternTask/SparInternTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparInternTaskApp.swift
+//  SparInternTask
+//
+//  Created by Azimjon Abdurasulov on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SparInternTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
